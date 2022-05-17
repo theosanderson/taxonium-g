5707cf5899932f5524e58b842b9c0464e213fc05
Mona Lisa <theo@theo.io>
2022-05-17 16:23:09 +0000

    updates

--- fullPipeline.sh
+++ fullPipeline.sh
@@ -1,3 +1,6 @@
+#! /bin/bash
+source /home/ubuntu/miniconda3/bin/activate base
+cd /mnt/data/taxonium-g 
 source pipeline1.sh
 source pipeline2.sh
 source pipeline3.sh
--- pipeline3.sh
+++ pipeline3.sh
@@ -1,2 +1,4 @@
 wget -qN https://raw.githubusercontent.com/theosanderson/taxonium/master/taxoniumtools/test_data/hu1.gb
-usher_to_taxonium --input /mnt/data/amended_data/sheared.pb --output /mnt/data/amended_data/gisaid.jsonl.gz --metadata /mnt/data/amended_data/amended_data.csv --genbank hu1.gb --columns name2,date,pangolin_lineage,continent,country,subregion --chronumental --chronumental_steps=300 --chronumental_reference_node=EPI_ISL_402124
+usher_to_taxonium --input /mnt/data/amended_data/sheared.pb --output /mnt/data/amended_data/gisaid_temp.jsonl.gz --metadata /mnt/data/amended_data/amended_data.csv --genbank hu1.gb --columns name2,date,pangolin_lineage,continent,country,subregion --chronumental --chronumental_steps=300 --chronumental_reference_node=EPI_ISL_402124
+mv /mnt/data/amended_data/gisaid_temp.jsonl.gz mv /mnt/data/amended_data/gisaid.jsonl.gz
+microk8s kubectl rollout restart deploy
